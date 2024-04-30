318763f5da4ebb01b06841d6e0cccc6db60fb61b
Shplong <kristianhybben@gmail.com>
2024-04-30 15:03:39 +0200

    Made something ressembling an account statement

--- app.py
+++ app.py
@@ -6,11 +6,70 @@ from datetime import datetime
 import sqlite3
 from payment_data import *
 
+class SettingsTab:
+    def __init__(self,frame,balance_value,iban,account_number,bic_swift,monthly_fee):
+        self.frame = frame
+        self.balance_value = balance_value
+        self.iban = iban
+        self.account_number = account_number
+        self.bic_swift = bic_swift
+        self.monthly_fee = monthly_fee
+    def create(self):
+        current_balance_detail = ttk.Label(self.frame, text=f"Current Balance: {self.balance_value}€")
+        current_balance_detail.pack()
+
+        your_iban = ttk.Label(self.frame, text=f"IBAN: {self.iban}")
+        your_iban.pack()
+
+        account_number_label = ttk.Label(self.frame, text=f"Account Number: {self.account_number}")
+        account_number_label.pack()
+
+        bic_swift_label = ttk.Label(self.frame, text=f"BIC/SWIFT: {self.bic_swift}")
+        bic_swift_label.pack()
+
+        monthly_fee_label = ttk.Label(self.frame, text=f"Monthly Fee: {self.monthly_fee}")
+        monthly_fee_label.pack()
+
 balance_value = 5000
 
 def new_window(tab_name):
     frame = ttk.Frame(root)
     notebook.add(frame, text=tab_name)
+    settings_tab = SettingsTab(
+                frame,
+                balance_value=balance_value,
+                iban="SK73 0100 0000 0015 6153 4661",
+                account_number="6493683369/0200",
+                bic_swift="ZITASKBX",
+                monthly_fee=3
+            )
+    
+    history_table = ttk.Treeview(frame)
+
+    history_table["column"] = ("id",
+                        "name",
+                        "iban",
+                        "amount",
+                        "variable_symbol",
+                        "constant_symbol",
+                        "specific_symbol",
+                        "message_for_recipient",
+                        "sender_reference",
+                        "timestamp")
+    
+    def filling_the_history_table(history_table, db_cursor):        
+        for col in history_table["column"]:
+            history_table.heading(col, text=col.capitalize())
+            history_table.column(col, width=100)
+
+        db_cursor.execute("SELECT * FROM payments")
+        rows = db_cursor.fetchall()
+
+        for row in rows:
+            history_table.insert("", "end", values=row)
+
+
+
     if tab_name == "New Payment":
         payment_name = ttk.Label(frame, text="Name")
         payment_name.pack()
@@ -84,35 +143,25 @@ def new_window(tab_name):
                 messagebox.showinfo("Invalid Amount")
         sign_transfer_button = ttk.Button(frame, text="Sign and transfer", command=sign_and_transfer)
         sign_transfer_button.pack()
-    elif tab_name == "Settings":         #TURN THIS INTO A CLASS
-        details_label = ttk.Label(frame, text="Product details")
-        details_label.pack()
-        current_balance_detail = ttk.Label(frame, text=f"Current Balance: {balance_value}€")
-        current_balance_detail.pack()
-        your_iban_value = "SK73 0100 0000 0015 6153 4661"
-        your_iban = ttk.Label(frame, text=f"IBAN: {your_iban_value}")
-        your_iban.pack()
-        account_number_value = "6493683369/0200"
-        account_number = ttk.Label(frame, text=f"Account Number: {account_number_value}")
-        account_number.pack()
-        bic_swift_value = "ZITASKBX"
-        bic_swift = ttk.Label(frame, text=f"BIC/SWIFT: {bic_swift_value}")
-        bic_swift.pack()
-        monthly_fee_value = 3
-        monthly_fee = ttk.Label(frame, text=f"Monthly fee: {monthly_fee_value}")
-        monthly_fee.pack()
+    elif tab_name == "Settings":         
+            
+            settings_tab.create()
     elif tab_name == "Account Statements":
-        statement_label = ttk.Label(frame, text="Statements")
-        statement_label.pack()
-        def generate_statement():  #make a function to create and display the statements
-            pass
-        latest_statement = ttk.Button(frame, text="Statement", command=generate_statement)
-        latest_statement.pack()
-    elif tab_name == "History":
-        tree = ttk.Treeview(frame)
-
-        tree["column"] = ("id",
-                        "name",
+        def generate_statement():
+            generated_statement_frame = ttk.Frame(notebook)
+            notebook.add(generated_statement_frame, text="statement")
+            detail_table = ttk.Treeview(generated_statement_frame, columns=("Property","Value"))
+            detail_table.heading("Property")
+            detail_table.heading("Value")
+            
+            detail_table.insert("", "end", values=("Balance", settings_tab.balance_value))
+            detail_table.insert("", "end", values=("IBAN", settings_tab.iban))
+            detail_table.insert("", "end", values=("Account Number", settings_tab.account_number))
+            detail_table.insert("", "end", values=("BIC/SWIFT", settings_tab.bic_swift))
+            detail_table.insert("", "end", values=("Monthly Fee", settings_tab.monthly_fee))
+            detail_table.pack(expand=True, anchor="n", fill="x")
+            history_table = ttk.Treeview(generated_statement_frame, columns=("id",
+                         "name",
                         "iban",
                         "amount",
                         "variable_symbol",
@@ -120,18 +169,16 @@ def new_window(tab_name):
                         "specific_symbol",
                         "message_for_recipient",
                         "sender_reference",
-                        "timestamp")
-        for col in tree["column"]:
-            tree.heading(col, text=col.capitalize())
-            tree.column(col, width=100)
-
-        db_cursor.execute("SELECT * FROM payments")
-        rows = db_cursor.fetchall()
-
-        for row in rows:
-            tree.insert("", "end", values=row)
-        
-        tree.pack(expand=True, fill="both")
+                        "timestamp"))
+            
+            filling_the_history_table(history_table, db_cursor)
+            history_table.pack(expand=True, fill="both")
+            
+        latest_statement = ttk.Button(frame, text="Statement", command=generate_statement)
+        latest_statement.pack()
+    elif tab_name == "History":
+        filling_the_history_table(history_table, db_cursor)
+        history_table.pack(expand=True, fill="both")
 
 root = tk.Tk()
 
